c2615ac03d5deac121165cd79277636dd39d574e
NOVABASE\NB26923 <joao.soares.pinto@celfocus.com>
2022-02-23 10:39:36 +0000

    Task 1, Hacker news w pagination

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,7 +1,85 @@
-import React from 'react';
+import {React} from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import {loadTopstories, fetchStoryById} from '../state/actions';
 
 function NewsList() {
-    return <div>NewsList</div>;
+const [allStory, setAllStory] = useState([]);
+const [pagStory, setPagStory] = useState([]);
+const [numPages, setNumPages] = useState(0);
+const [currPage, setCurrPage] = useState(0);
+const [pages, setPages] = useState([]);
+const perPage=17;
+
+useEffect(()=>{
+    loadTopstories().then((sData) => 
+    {
+        setNumPages(Math.round((sData.length)/perPage)+0.5)
+        let saveStories=[];
+        sData.forEach((story,idx) =>{
+        fetchStoryById(story).then((idata) => {
+            saveStories.push(idata)
+            if(idx==sData.length-1)
+            {
+                setAllStory(saveStories);
+                setPagStory(saveStories.slice(0,perPage))
+
+            }    
+        })
+            
+    })
+}    )
+}, []);
+
+  useEffect(() => {
+    renderPageNumbers()
+  }, [numPages]);
+
+  useEffect(() => {
+    setCurrPage(1)
+  }, [allStory]);
+
+  useEffect(() => {
+    setPagStory(allStory.slice(currPage*perPage-perPage,currPage*perPage))
+  }, [currPage]);
+
+const handleClick=(event) => {
+    setCurrPage(event.target.id)
+  };
+
+
+    let renderPageNumbers = () => {
+        let i = 1;
+        let auxArr = [];
+        for (i; i < numPages + 1; i++) {
+            auxArr.push(
+                <span
+                    key={i}
+                    id={i}
+                >
+                    <button
+                        key={i}
+                        id={i}
+                        onClick={handleClick}>
+
+                        {i}
+                    </button>
+                </span>)
+        }
+        setPages(auxArr)
+    }
+
+
+    return (<>
+        <ul>
+            {pagStory.map((story, idx) => {
+                return <li key={idx}><p>{story.data.title}</p></li>
+            })}
+        </ul>
+        <ul id="page-numbers">
+            {pages}
+        </ul>
+    </>);
 }
 
 export default NewsList;
--- src/state/actions.js
+++ src/state/actions.js
@@ -1,10 +1,25 @@
 // Find Hacker News API here
 // https://github.com/HackerNews/API
 
+import axios from 'axios';
+
 // TODO fetch ask stories (https://hacker-news.firebaseio.com/v0/askstories.json)
-export function loadTopstories() {
-    return async (dispatch, getState) => {};
-}
+
+export const loadTopstories = async () => {
+    return  await axios
+            .get(
+                `https://hacker-news.firebaseio.com/v0/askstories.json`
+            )
+            .then(res1 => res1.data)
+
+    };
 
 // TODO fetch item by id (https://hacker-news.firebaseio.com/v0/item/<itemId>.json)
-function fetchStoryById(id) {}
+
+export const fetchStoryById = async (id) => {
+
+    return await axios
+        .get(
+            `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+        ).then(res2 => res2)
+}
