2a297a9884160be793b475b40aa06264d4f03879
NOVABASE\NB26923 <joao.soares.pinto@celfocus.com>
2022-02-23 10:40:06 +0000

    Task 2 product values updates

--- src/Task2.js
+++ src/Task2.js
@@ -1,71 +1,12 @@
 /* eslint-disable */
 
-import { useEffect } from 'react';
-
-
-export function updateQuality(products) {
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].type == "TICKETS" && products[i].sellIn >= 10) {
-      products[i].quality = products[i].quality + 1;
-      products[i].sellIn = products[i].sellIn - 1;
-    } else {
-      if (
-        products[i].type == "TICKETS" &&
-        products[i].sellIn < 10 &&
-        products[i].sellIn > 1
-      ) {
-        products[i].quality = products[i].quality + 2;
-        products[i].sellIn = products[i].sellIn - 1;
-      } else {
-        if (products[i].type == "TICKETS" && products[i].sellIn <= 1) {
-          products[i].quality = 0;
-          products[i].sellIn = 0;
-        }
-
-        if (
-          products[i].type == "NORMAL" &&
-          products[i].sellIn <= 0 &&
-          products[i].quality > 0
-        ) {
-          products[i].quality = products[i].quality - 2;
-          products[i].sellIn = products[i].sellIn - 1;
-        }
-
-        if (
-          products[i].type == "NORMAL" &&
-          products[i].sellIn > 0 &&
-          products[i].quality == 0
-        ) {
-          products[i].quality = 0;
-          products[i].sellIn = products[i].sellIn - 1;
-        } else {
-          if (
-            products[i].type == "NORMAL" &&
-            products[i].sellIn > 0 &&
-            products[i].quality > 0
-          ) {
-            products[i].quality = products[i].quality - 1;
-            products[i].sellIn = products[i].sellIn - 1;
-          }
-        }
-      }
-    }
-
-    if (products[i].sellIn <= 0) {
-      products[i].sellIn = 0;
-    }
-
-    if (products[i].quality <= 0) {
-      products[i].quality = 0;
-    }
-  }
-
-  return products;
-}
+import { useEffect,useState } from 'react';
 
 export function Task2() {
+  const [products, setProds] = useState([]);
+  const [numDay, setDay] = useState(1);
     useEffect(() => {
-        const products = [
+      setProds( [
             {
                 name: 'concert a',
                 type: 'TICKETS',
@@ -112,10 +53,65 @@ export function Task2() {
                 sellIn: 5,
                 isSecondHand: true,
             },
-        ];
-
-        const updated = updateQuality(products);
-        console.log(updated);
+        ])
     }, []);
-    return null;
+
+  const updateQuality2= () => {
+
+
+
+      for (let i = 0; i < products.length; i++) {
+
+        let auxSecHand= products[i].isSecondHand? 2: 1;
+
+        if (products[i].type == "TICKETS")
+        {
+          if(products[i].sellIn >= 10)
+          {
+            products[i].quality = products[i].quality + 1;
+          }
+          else if(products[i].sellIn > 1)
+          {
+            products[i].quality = products[i].quality + 2;
+          }
+          else
+          {
+            products[i].quality = 0;
+          }
+        }
+
+        if (products[i].type == "NORMAL" && products[i].quality > 0)
+        {
+          if(products[i].sellIn <= 0)
+          {
+            products[i].quality = products[i].quality - 2*auxSecHand;
+          }
+          else if(products[i].sellIn > 0)
+          {
+            products[i].quality =   products[i].quality - 1*auxSecHand;
+          }
+        }
+    
+        if (products[i].sellIn > 0) {
+          products[i].sellIn = products[i].sellIn - 1;
+        }
+    
+        if (products[i].quality <= 0) {
+          products[i].quality = 0;
+        }
+      }
+    
+    setDay(numDay+1)  
+    }
+
+    return (<><button onClick={updateQuality2}>update</button>
+    <div>Day: {numDay}</div>
+    <ul>
+      {products.map((prod, idx) => {
+        return <li key={idx}><p>{prod.name} -  {prod.type} - Quality; {prod.quality} - SellIn: {prod.sellIn} - SecondHand: {prod.isSecondHand?'Yes':'No'}
+        </p></li>;
+      })}
+    </ul>
+    </>
+      );
 }
